1af6611b81587ecd47dd1b98ac3b116fe52a608f
Surbhi <surbhikohli1995@gmail.com>
2019-04-29 16:32:34 +0530

    Updates

---
+++ precache-manifest.43df8e30195f159ede9ba1a0e8ac5a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1218923966e43481f835b666ba9aa152",
+    "url": "/repo22/index.html"
+  },
+  {
+    "revision": "6fe1fb8209608f5ffe47",
+    "url": "/repo22/static/css/main.45bab136.chunk.css"
+  },
+  {
+    "revision": "f04ec492e7e4ab42679b",
+    "url": "/repo22/static/js/2.fed26185.chunk.js"
+  },
+  {
+    "revision": "6fe1fb8209608f5ffe47",
+    "url": "/repo22/static/js/main.2790599f.chunk.js"
+  },
+  {
+    "revision": "639272b11b4ad499c07d",
+    "url": "/repo22/static/js/runtime~main.6390185b.js"
+  }
+]);
